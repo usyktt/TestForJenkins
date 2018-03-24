e4c2ba07bfb92fdd80eafb32a1be0c83ee7746e3
usyktt <usyktt@gmail.com>
2018-03-24 10:42:28 +0200

    test

--- WebApplication4/Controllers/ValuesController.cs
+++ WebApplication4/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ namespace WebApplication4.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
+        // GET api/all values
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
